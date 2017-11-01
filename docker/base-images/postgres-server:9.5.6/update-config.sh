d0bd6dc131b74795e8ea0b6964c1d7a4476294f7
Eder Ruiz Maria <drrzmr@gmail.com>
2017-11-01 17:40:36 -0200

    docker: Enable remote access to replication protocol

--- docker/base-images/postgres-server:9.5.6/update-config.sh
+++ docker/base-images/postgres-server:9.5.6/update-config.sh
@@ -14,3 +14,18 @@ max_wal_senders = 2
 max_replication_slots = 2
 wal_level = logical
 EOF
+
+
+###############
+# pg_hba.conf #
+###############
+
+# open replication protocol, except for ipv6
+cat << EOF >> ${PGDATA}/pg_hba.conf
+
+###########################
+# open replication access #
+###########################
+local replication postgres           trust
+host  replication postgres 0.0.0.0/0 trust
+EOF
