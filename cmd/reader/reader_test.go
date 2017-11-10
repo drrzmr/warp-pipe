c74099c5e0ab345b33464fed82f7f61886168ed5
Eder Ruiz Maria <drrzmr@gmail.com>
2017-11-09 23:31:33 -0200

    cmd: Add test for reader command

---
+++ cmd/reader/reader_test.go
@@ -0,0 +1,66 @@
+package reader
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/pagarme/warp-pipe/config"
+)
+
+func Test_CmdReader(t *testing.T) {
+
+	var err error
+
+	// sender
+	sr, sw := io.Pipe()
+	require.NotNil(t, sr)
+	require.NotNil(t, sw)
+
+	// receiver
+	rr, rw := io.Pipe()
+	require.NotNil(t, rr)
+	require.NotNil(t, rw)
+
+	text := "testing...\n"
+
+	confReader := &config.Reader{
+		InputStream:  sr,
+		OutputStream: rw,
+	}
+
+	buf := &bytes.Buffer{}
+	readerCmd := New(confReader)
+	readerCmd.SetOutput(buf)
+	out := readerCmd.OutOrStdout()
+	require.Equal(t, buf, out)
+
+	// buf -> sw -> sr
+	go func() {
+		var errSender error
+		buf := []byte(text)
+
+		n, errSender := sw.Write(buf)
+		require.Equal(t, len(buf), n)
+		require.NoError(t, errSender)
+
+		errSender = sw.Close()
+		require.NoError(t, errSender)
+	}()
+
+	// sr -> rw -> rr
+	go func() {
+		errCmd := readerCmd.Execute()
+		require.NoError(t, errCmd)
+	}()
+
+	// rr -> receivedBuf
+	receivedBuf, err := ioutil.ReadAll(rr)
+	receivedText := string(receivedBuf)
+	require.NoError(t, err)
+	require.Equal(t, fmt.Sprintf("-> %s", text), receivedText)
+}
