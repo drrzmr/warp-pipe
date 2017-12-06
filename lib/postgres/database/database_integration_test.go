156b4d1d54e8dab9089530cf63af224757ccbd7f
Eder Ruiz Maria <drrzmr@gmail.com>
2017-12-06 05:31:44 -0200

    lib postgres: Add integration test ConnectSuccessful with docker

--- lib/postgres/database/database_integration_test.go
+++ lib/postgres/database/database_integration_test.go
@@ -7,8 +7,10 @@ import (
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/require"
 
+	"github.com/pagarme/warp-pipe/lib/docker"
 	"github.com/pagarme/warp-pipe/lib/postgres"
 	"github.com/pagarme/warp-pipe/lib/postgres/database"
+	dockerTester "github.com/pagarme/warp-pipe/lib/tester/docker"
 )
 
 func TestDatabaseIntegration(t *testing.T) {
@@ -34,5 +36,32 @@ func TestDatabaseIntegration(t *testing.T) {
 		err = d.Connect()
 		require.Error(t, err)
 		require.IsType(t, &net.OpError{}, errors.Cause(err))
+	})
+
+	t.Run("ConnectSuccessful", func(t *testing.T) {
+
+		ipAddress, deferFn := dockerTester.Run(t, docker.Config{
+			WaitTimeout: docker.DefaultWaitTimeout,
+			URL:         "warp-pipe",
+			Image:       "postgres-server",
+			Tag:         "9.5.6",
+		})
+		defer deferFn()
+
+		var err error
+
+		d := database.New(postgres.Config{
+			Host:     ipAddress,
+			Port:     postgres.DefaultPort,
+			User:     postgres.DefaultUser,
+			Database: "postgres",
+			Password: "postgres",
+
+			Driver:         "pgx",
+			ConnectTimeout: postgres.MinConnectTimeout,
+		})
+
+		err = d.Connect()
+		require.NoError(t, err)
 	})
 }
