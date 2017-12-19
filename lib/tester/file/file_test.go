213b09b74595fde36d19dccece60a21d6bece615
Jonatas Pedraza <jonatas.pedraza@pagar.me>
2017-12-19 00:32:12 -0200

    lib tester: Add LoadJSON func file helper

--- lib/tester/file/file_test.go
+++ lib/tester/file/file_test.go
@@ -11,3 +11,12 @@ func TestLoad(t *testing.T) {
 	text := file.Load(t, "loremipson")
 	file.MustMatch(t, "loremipson", text)
 }
+
+func TestLoadJson(t *testing.T) {
+	sampleTable := make([]struct {
+		Name string
+		Age  int
+	}, 0)
+
+	file.LoadJSON(t, "sample", &sampleTable)
+}
