fae13dce81b1c49a1bc6d5af4ad049cd4db3e33a
Jonatas Pedraza <jonatas.pedraza@pagar.me>
2017-12-19 12:53:32 -0200

    lib parser: Test transaction

--- lib/parser/testdecoding/parser_test.go
+++ lib/parser/testdecoding/parser_test.go
@@ -11,7 +11,7 @@ import (
 
 func TestParser(t *testing.T) {
 
-	t.Run("NewParser", func(t *testing.T) {
+	/*t.Run("NewParser", func(t *testing.T) {
 		parser := testdecoding.NewParser(nil)
 		require.NotNil(t, parser)
 	})
@@ -51,5 +51,57 @@ func TestParser(t *testing.T) {
 			err := parser.Parse(line)
 			require.NoError(t, err)
 		})
+	})*/
+
+	t.Run("Transaction", func(t *testing.T) {
+
+		table := []struct {
+			filename    string
+			expectError bool
+			expectNil   bool
+			expectLen   bool
+		}{
+			{filename: "transaction-without-begin", expectError: true, expectNil: true, expectLen: false},
+			{filename: "transaction-without-commit", expectError: false, expectNil: true, expectLen: false},
+			{filename: "transaction-invalid", expectError: true, expectNil: true, expectLen: false},
+			{filename: "transaction-without-begin-id", expectError: true, expectNil: true, expectLen: false},
+			{filename: "transaction-without-commit-id", expectError: true, expectNil: true, expectLen: false},
+			{filename: "transaction-without-operations", expectError: false, expectNil: false, expectLen: false},
+			{filename: "transaction-multiples", expectError: false, expectNil: false, expectLen: true},
+			{filename: "transaction-messy", expectError: true, expectNil: true, expectLen: false},
+			{filename: "transaction-messy-but-valid", expectError: true, expectNil: false, expectLen: true},
+		}
+
+		for _, tt := range table {
+			t.Run(tt.filename, func(t *testing.T) {
+				parser := testdecoding.NewParser(func(transaction testdecoding.Transaction) {
+					if tt.expectLen {
+						require.True(t, len(transaction.Operations) > 0)
+					} else {
+						require.Len(t, transaction.Operations, 0)
+					}
+
+					if tt.expectNil {
+						require.Nil(t, transaction)
+					} else {
+						require.NotNil(t, transaction)
+					}
+				})
+
+				var errList []error
+				file.ForEachLine(t, tt.filename, func(line string) {
+					err := parser.Parse(line)
+					if err != nil {
+						errList = append(errList, err)
+					}
+				})
+
+				if tt.expectError {
+					require.True(t, len(errList) > 0)
+				} else {
+					require.True(t, len(errList) == 0)
+				}
+			})
+		}
 	})
 }
