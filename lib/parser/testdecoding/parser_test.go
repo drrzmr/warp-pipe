5c9362a047d4dcfaa7e9f9e931a95a9d7306b113
Jonatas Pedraza <jonatas.pedraza@pagar.me>
2017-12-19 12:13:29 -0200

    lib parser: Improve state machine

--- lib/parser/testdecoding/parser_test.go
+++ lib/parser/testdecoding/parser_test.go
@@ -12,60 +12,83 @@ import (
 func TestParser(t *testing.T) {
 
 	t.Run("NewParser", func(t *testing.T) {
-		parser := testdecoding.NewParser()
+		parser := testdecoding.NewParser(nil)
 		require.NotNil(t, parser)
-		fsm := parser.FSM()
-		require.NotNil(t, fsm)
-	})
-
-	t.Run("FSMTransitions", func(t *testing.T) {
-		parser := testdecoding.NewParser()
-		fsm := parser.FSM()
-
-		require.Equal(t, fsm.Current(), "idle")
-		fsm.Event("begin")
-		require.Equal(t, fsm.Current(), "parsing")
-		fsm.Event("parse")
-		require.Equal(t, fsm.Current(), "parsing")
-		fsm.Event("parse")
-		require.Equal(t, fsm.Current(), "parsing")
-		fsm.Event("commit")
-		require.Equal(t, fsm.Current(), "idle")
-	})
-
-	t.Run("Append", func(t *testing.T) {
-		parser := testdecoding.NewParser()
-
-		testTable := []struct {
-			line string
-		}{
-			{"BEGIN 627"},
-			{"COMMIT 627"},
-		}
-		var (
-			readLine string
-			err      error
-		)
-
-		for i, testCase := range testTable {
-			parser.Append(testCase.line)
-			readLine, err = parser.Log(uint(i))
-			require.NoError(t, err)
-			require.Equal(t, readLine, testCase.line)
-		}
 	})
 
 	t.Run("Parse", func(t *testing.T) {
-		parser := testdecoding.NewParser()
+		operationTable := []struct {
+			Table string
+			Type  string
+			Value string
+		}{
+			{
+				Table: "table_with_pk",
+				Type:  "INSERT",
+				Value: "a[integer]:1 " +
+					"b[character varying]:'Backup and Restore' " +
+					"c[timestamp without time zone]:'2017-11-30 17:59:33.825033'",
+			}, {
+				Table: "table_with_pk",
+				Type:  "INSERT",
+				Value: "a[integer]:2 " +
+					"b[character varying]:'Tuning' " +
+					"c[timestamp without time zone]:'2017-11-30 17:59:33.825033'",
+			}, {
+				Table: "table_with_pk",
+				Type:  "INSERT",
+				Value: "a[integer]:3 " +
+					"b[character varying]:'Replication' " +
+					"c[timestamp without time zone]:'2017-11-30 17:59:33.825033'",
+			}, {
+				Table: "table_with_pk",
+				Type:  "DELETE",
+				Value: "a[integer]:1 " +
+					"c[timestamp without time zone]:'2017-11-30 17:59:33.825033'",
+			}, {
+				Table: "table_with_pk",
+				Type:  "DELETE",
+				Value: "a[integer]:2 " +
+					"c[timestamp without time zone]:'2017-11-30 17:59:33.825033'",
+			}, {
+				Table: "table_without_pk",
+				Type:  "INSERT",
+				Value: "a[integer]:1 " +
+					"b[numeric]:2.34 " +
+					"c[text]:'Tapir'",
+			}, {
+				Table: "table_without_pk",
+				Type:  "UPDATE",
+				Value: "a[integer]:1 " +
+					"b[numeric]:2.34 " +
+					"c[text]:'Anta'",
+			},
+		}
 
-		file.ForEachLine(t, "transaction", func(line string) {
-			parser.Append(line)
+		parser := testdecoding.NewParser(func(transaction testdecoding.Transaction) {
+
+			require.Equal(t, uint64(627), transaction.ID)
+			for i, op := range operationTable {
+				require.Equal(t, op.Table, transaction.Operations[i].Table)
+				require.Equal(t, op.Type, transaction.Operations[i].Type)
+				require.Equal(t, op.Value, transaction.Operations[i].Value)
+			}
+
 		})
 
-		transaction, err := parser.Parse()
+		file.ForEachLine(t, "transaction", func(line string) {
+			err := parser.Parse(line)
+			require.NoError(t, err)
+		})
 
-		require.NoError(t, err)
-		require.NotNil(t, transaction)
-		require.NotEmpty(t, transaction)
+		file.ForEachLine(t, "transaction", func(line string) {
+			err := parser.Parse(line)
+			require.NoError(t, err)
+		})
+
+		file.ForEachLine(t, "transaction", func(line string) {
+			err := parser.Parse(line)
+			require.NoError(t, err)
+		})
 	})
 }
